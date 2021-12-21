e02546d430cf8bbb75666d8065bbb1a917fe6717
lizongxin <silver.codingcat@gmail.com>
2021-12-21 23:05:05 +0800

    add files

---
+++ src/build_util.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@author: Silver (silver.codingcat@gmail.com)
+@create: 2021/12/20
+@brief:
+"""
+import json
+import time
+
+import datetime
+
+SCHEDULE_NAME_TO_TYPE = {
+    "开发": 1,
+    "联调": 2,
+    "提测": 3,
+    "测试": 4,
+    "上线": 5,
+}
+
+SCHEDULE_NAME_TO_SUB_TYPE = {
+    "before": 1,
+    "start": 2,
+    "end": 3,
+}
+
+
+def get_0_hour_stamp(timestamp):
+    date = datetime.date.fromtimestamp(timestamp)
+    return int(time.mktime(date.timetuple()))
+
+
+def build_project(project):
+    return project
+
+
+def build_robot(robot):
+    return robot
+
+
+def get_schedule_date(schedule):
+    schedule_date = {}
+    for name, str_date in schedule.items():
+        schedule_type = SCHEDULE_NAME_TO_TYPE.get(name)
+        if not schedule_type or not str_date:
+            continue
+        schedule_date[schedule_type] = datetime.datetime.strptime(str_date, "%Y-%m-%d")
+    return schedule_date
+
+
+def build_notices_from_schedule(project_id, robot_id, schedule_date, notice):
+    db_notice_list = []
+
+    for schedule_sub_type_name, notice_schedule_types in notice.items():
+        schedule_sub_type = SCHEDULE_NAME_TO_SUB_TYPE.get(schedule_sub_type_name)
+        if not schedule_sub_type:
+            continue
+        notices = build_notices_from_sub_type(
+            schedule_sub_type, notice_schedule_types, schedule_date, project_id, robot_id)
+        db_notice_list.extend(notices)
+    return db_notice_list
+
+
+def build_notices_from_sub_type(schedule_sub_type, notice_schedule_types, schedule_date, project_id, robot_id):
+    db_notices = []
+    if schedule_sub_type not in (1, 2):
+        return db_notices
+
+    now = int(time.time())
+    today = get_0_hour_stamp(now)
+    for schedule_type in notice_schedule_types:
+        # 跳过未输入排期
+        if schedule_type not in schedule_date:
+            continue
+
+        notice_start_date = schedule_date[schedule_type]
+        notice_time = get_notice_time_from_schedule_sub_type(schedule_sub_type, notice_start_date)
+        if today >= notice_time:
+            continue
+        db_notice = build_db_notice(project_id, robot_id, schedule_type, schedule_sub_type, notice_time)
+        db_notices.append(db_notice)
+    return db_notices
+
+
+def get_notice_time_from_schedule_sub_type(schedule_sub_type, start_date):
+    if schedule_sub_type not in (1, 2):
+        return 0
+    if schedule_sub_type == 2:
+        return int(start_date.timestamp())
+    elif schedule_sub_type == 1:
+        return int(start_date.timestamp()) - 86400
+    return 0
+
+
+def build_db_notice(project_id, robot_id, schedule_type, schedule_sub_type, notice_time):
+    return {
+        "project_id": project_id,
+        "robot_id": robot_id,
+        "type": schedule_type,
+        "sub_type": schedule_sub_type,
+        "notice_time": notice_time,
+    }
---
+++ src/project_robot.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@author: Silver (silver.codingcat@gmail.com)
+@create: 2021/12/20
+@brief:
+"""
+import time
+import datetime
+import json
+
+import torndb
+import requests
+
+
+def get_0_hour_stamp(timestamp):
+    date = datetime.date.fromtimestamp(timestamp)
+    return int(time.mktime(date.timetuple()))
+
+
+class Script(object):
+    def __init__(self):
+        self.now = int(time.time())
+        config = {
+        }
+        self.db = torndb.Connection(**config)
+
+    def get_notices(self, start, end):
+        sql = """
+        SELECT
+        n.id AS notice_id,
+        n.type,
+        n.sub_type,
+        n.project_id,
+        p.name AS project_name,
+        p.url AS project_url,
+        n.robot_id,
+        r.webhook
+        FROM notice n
+        JOIN project p ON n.project_id = p.id
+        JOIN robot r ON n.robot_id = r.id
+        WHERE n.notice_time >= %s
+        AND n.notice_time < %s
+        AND n.status = 1
+        """
+        params = [start, end, ]
+        return self.db.query(sql, *params)
+
+    @staticmethod
+    def get_content(notice_type, sub_type):
+        notice_type_map = {
+            1: "开发",
+            2: "联调",
+            3: "提测",
+            4: "测试",
+            5: "上线",
+        }
+        notice_sub_type_map = {
+            1: "即将开始",
+            2: "进行中",
+            3: "即将结束",
+        }
+        return f"项目{notice_type_map[notice_type]}{notice_sub_type_map[sub_type]}，请关注。"
+
+    def build_notice_content(self, notice):
+        data = {
+            "msgtype": "news",
+            "news": {
+                "articles": [
+                    {
+                        "title": notice["project_name"],
+                        "description": self.get_content(notice["type"], notice["sub_type"]),
+                        "url": notice["project_url"],
+                        "picurl": ""
+                    }
+                ]
+            }
+        }
+
+        return data
+
+    @staticmethod
+    def send_notice(webhook, content, is_json=True):
+        response = requests.post(webhook, data=json.dumps(content) if is_json else content)
+        return response
+
+    def run(self):
+        today = get_0_hour_stamp(self.now)
+        tomorrow = today + 86400
+        notices = self.get_notices(today, tomorrow)
+        for notice in notices:
+            content = self.build_notice_content(notice)
+            self.send_notice(notice["webhook"], content)
+
+
+def main():
+    s = Script()
+    s.run()
+
+
+if __name__ == '__main__':
+    main()
